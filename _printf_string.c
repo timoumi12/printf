dbe637975c1ee78e2319f84b8e3435e79685d5d0
timoumi12 <ihebtimoumi1@gmail.com>
2023-04-24 11:23:49 +0100

    prints normal string

---
+++ _printf_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _print_string - prints s
+ * @s: string
+ * Return: void
+*/
+
+int _printf_string(va_list args)
+{
+	int i;
+	char *s;
+
+	s = va_arg(args, char *);
+	if (s == NULL)
+	{
+		s = "(null)";
+	}
+	for (i = 0; s[i] != '\0'; i++)
+		_putchar(s[i]);
+	return (i);
+}
