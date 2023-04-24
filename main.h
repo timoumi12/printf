424377d5724cecca2e2d912970aa95bab5f0bf9e
timoumi12 <ihebtimoumi1@gmail.com>
2023-04-24 15:32:06 +0100

    header file for _printf fn

---
+++ main.h
@@ -0,0 +1,34 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.>
+#include <string.h>
+#include <limits.h>
+#include <unistd.h>
+
+/**
+ * struct specifier - struct specifier
+ * @valid: the valid character.
+ * @f: the functions associated.
+ *
+*/
+typedef struct specifier
+{
+	char valid;
+	int (*f)(va_list);
+} spec;
+
+int _putchar(char c);
+
+int _printf(const char *format, ...);
+
+int _printf_string(va_list args);
+
+int _printf_char(va_list args);
+
+int _printf_integer(va_list args);
+
+int _printf_decimal(va_list args);
+
+#endif
