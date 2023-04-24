78fe7ec574517723d22f513960ea42b1abeb5523
asmarais <rais.asma99@gmail.com>
2023-04-24 07:35:01 -0700

    decimal function

---
+++ _printf_decimal.c
@@ -0,0 +1,35 @@
+/**
+ * _printf_decimal - prints a decimal
+ * @args: decimal argument
+ * Return: counter
+ */
+int _printf_decimal(va_list args)
+{
+
+	unsigned int absolute, aux, countnum, count;
+	int n;
+
+	count = 0;
+	n = va_arg(args, int);
+		if (n < 0)
+		{
+			absolute = (n * -1);
+			count += _putchar('-');
+		}
+		else
+			absolute = n;
+
+	aux = absolute;
+	countnum = 1;
+	while (aux > 9)
+	{
+		aux /= 10;
+		countnum *= 10;
+	}
+	while (countnum >= 1)
+	{
+		count += _putchar(((absolute / countnum) % 10) + '0');
+		countnum /= 10;
+	}
+	return (count);
+}
